941f569f8da183c39ee4c00186aa3d82efa6ea6e
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2022-03-16 10:24:37 +0900

    fix plugins on 5.6

--- Plugins/DistributedActorsGeneratorPlugin/Plugin.swift
+++ Plugins/DistributedActorsGeneratorPlugin/Plugin.swift
@@ -15,10 +15,14 @@
 import PackagePlugin
 
 @main struct MyPlugin: BuildToolPlugin {
-    func createBuildCommands(context: TargetBuildContext) throws -> [Command] {
+    func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
         let generatorPath = try context.tool(named: "DistributedActorsGenerator").path
 
-        let inputFiles = context.inputFiles.map { $0.path }.filter { $0.extension?.lowercased() == "swift" }
+        guard let sourceTarget = target as? SourceModuleTarget else {
+          return []
+        }
+
+        let inputFiles = sourceTarget.sourceFiles.map { $0.path }.filter { $0.extension?.lowercased() == "swift" }
 
         let buckets = 5 // # of buckets for consistent hashing
         let outputFiles = !inputFiles.isEmpty ? (0 ..< buckets).map {
@@ -26,10 +30,10 @@ import PackagePlugin
         } : []
 
         let command = Command.buildCommand(
-            displayName: "Generating distributed actors for target '\(context.targetName)'",
+            displayName: "Generating distributed actors for target '\(target.name)'",
             executable: generatorPath,
             arguments: [
-                "--source-directory", context.targetDirectory.string,
+                "--source-directory", target.directory.string,
                 "--target-directory", context.pluginWorkDirectory.string,
                 "--buckets", "\(buckets)",
             ],
